d679fbb6610db5f9144b4b6e721e02e2b05bf484
navyxie <navyxie2010@gmail.com>
2016-08-12 13:17:05 +0800

    去掉测试配置信息

--- test/index.js
+++ test/index.js
@@ -1,14 +1,9 @@
 var index = require('../lib/index');
 var util = require('../lib/util');
 var should = require('should');
-var serverDomain = 'http://pay.kaolalicai.cn'; //'http://192.168.1.120:8120'
-var cryptoConfig = {
-    md5_key: 'hhhiljahmjkcookf', //md5 key
-    des_key: 'gdlsmroisfhocawdivkktpuj', //des key,24个字符长度
-    aes_key: 'objfkjcdhkokgakg', //aes key,16个字符长度
-    merchant_pri_key: 'MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAIQ52ioNZvU3wSlQlrDS0Dodzw7GYjcXzJnTglbGBmZI6zu3FXOGJPJbmziKGmi7UIogjd0D1IaobDMPzXbgsS5oJEwEpfEsrrBlRDu/IZGf9CYa2FfC92b24yuvtLQx+OYT7xpcht01smZwadR2/yG95fzRXi4Yo7ppTi3lpePvAgMBAAECgYA6fExfZWYw+ORbneXJeLXZvu2jH2S58bDyKXfxYc3a2E4UL7sxShh1isdxjsR3psLNFfEp3VPX7A9P1qGO/3ve4q7DqH98MgJbncPCyxK/K4WyL6roYNtn/rsbXW0ifDVvPBdWeRVkm1BDEKUvZUf5Q2ZJlb17uDBoT8sRMYMA4QJBANMFuf9IXs76IMpYYGPrhJX1qWvHGTqQi3uAtWnsQevvZ5hepIK21i627/uYdK6tKpdJpUX0NboZpn9tk15ahYMCQQCgaKlXifgWtwT0H1IF7E7UU+xMdzoeiOcw5GducAdeTljrNCs9pGLdrgJn4wA99TcGHZ9lM1hMEDPGYZoHYYglAkAWBVf2it/R64o2c2iDpNUuLPekFp/MW78igTjtN8ldTJtb7Cxws5HASDjc4XjzrYBvTzEfDq/4nqqgS5gAVaPvAkAjvC09mzbxmIQ5/mA/gY1uL+QMhOMlukoG0ltYdwytLbcRbuXTLbP4vrM6gY+kPeSsMB06Zl3dVB60qzmtO0UhAkA8bzh4fdtX0ZJj4shuxsy0zxsC7t6+RNqY/FA85jEPI5JFhVrm27eunf0lQaPewvpTcjGkhCMzqZRYq+EmuUgM', //rsa pri key
-    npay_pub_key: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCEOdoqDWb1N8EpUJaw0tA6Hc8OxmI3F8yZ04JWxgZmSOs7txVzhiTyW5s4ihpou1CKII3dA9SGqGwzD8124LEuaCRMBKXxLK6wZUQ7vyGRn/QmGthXwvdm9uMrr7S0MfjmE+8aXIbdNbJmcGnUdv8hveX80V4uGKO6aU4t5aXj7wIDAQAB', //rsa pub  key
-};
+var testConfig = require('./test.config');
+var serverDomain = testConfig.serverDomain;
+var cryptoConfig = testConfig.cryptoConfig;
 describe('index', function() {
     this.timeout(5000);
     describe('init', function() {
